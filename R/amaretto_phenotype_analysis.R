0c21ced527be6851df4cf1e1b817e2244c44db40
Mohsen <monabiyan@gmail.com>
2019-05-17 12:36:24 -0400

    Fix the bugs for Survival test and Wilcoxon test

--- R/amaretto_phenotype_analysis.R
+++ R/amaretto_phenotype_analysis.R
@@ -74,6 +74,7 @@ AMARETTO_statistical_test<-function(AMARETTOinit,AMARETTOresults,sample_annotati
 #'
 #' @param results_list 
 #' @import tibble
+#' @import purrr
 #' @return
 #' @export
 #'
@@ -83,15 +84,16 @@ AMARETTO_unite_results<-function(results_list){
   for (i in 1:length(results_list)){
     if (results_list[[i]]$test_type=="COXPROPHAZARD"){
       df<-results_list[[i]]$test_result
-      statistics_str<-gsub("\\(","[",unlist(strsplit(df$"HR (95% CI for HR)"," "))[2])
+      Hazard_ratio<-unlist(purrr::map(strsplit(df$"HR (95% CI for HR)"," "),1))
+      statistics_str<-gsub("\\(","[",unlist(purrr::map(strsplit(df$"HR (95% CI for HR)"," "),1)))
       statistics_str<-gsub("\\)","]",statistics_str)
       statistics_str<-gsub("-",",",statistics_str)
       df<-add_column(df,
                      Phenotypes=paste0(results_list[[i]]$phenotype1," (COXPROPHAZARDTIMETOEVENT), ",results_list[[i]]$phenotype2," (COXPROPHAZARDRIGHTCENSORING)"),
                      Statistical_Test="Survival Analysis: Cox proportional hazards regression (Wald test)")%>%
         mutate(ModuleNr=paste("Module",ModuleNr))%>%
-        add_column(Descriptive_Statistics=paste0("Beta: ",df$beta,
-                                                 ", Hazard Ratio: ",unlist(strsplit(df$"HR (95% CI for HR)"," "))[1],
+        mutate(Descriptive_Statistics=paste0("Beta: ",df$beta,
+                                                 ", Hazard Ratio: ",Hazard_ratio,
                                                  ", 95% CI: ",statistics_str,
                                                  ", Wald Statistic: ",df$wald.test))%>%
         select(ModuleNr,Phenotypes,Statistical_Test,p.value,q.value,Descriptive_Statistics)
@@ -168,7 +170,7 @@ AMARETTO_unite_results<-function(results_list){
   return(united_df)
 }
 
-#' Title
+#' Title create_feature_matrix
 #'
 #' @param AMARETTOinit 
 #' @param AMARETTOresults 
@@ -189,7 +191,7 @@ create_feature_matrix<-function(AMARETTOinit,AMARETTOresults,sample_annotation_d
   return(feature_matrix)
 }
 
-#' Title
+#' Title cox_prop_hazard_test
 #'
 #' @param xx 
 #' @param time_to_event 
@@ -201,24 +203,36 @@ create_feature_matrix<-function(AMARETTOinit,AMARETTOresults,sample_annotation_d
 #' @examples
 cox_prop_hazard_test<-function(xx,time_to_event,right_censoring){
   df=data.frame(module_mean=xx,COXPROPHAZARDTIMETOEVENT=as.numeric(time_to_event),COXPROPHAZARDRIGHTCENSORING=as.numeric(right_censoring))
-  COXPROPHAZARD_formulas <- sapply(c("xx"), function(x) as.formula(paste('Surv(COXPROPHAZARDTIMETOEVENT, COXPROPHAZARDRIGHTCENSORING)~', x)))
-  COXPROPHAZARD_models <- lapply(COXPROPHAZARD_formulas, function(x){coxph(x, data = df)})
-  COXPROPHAZARD_res <- lapply(COXPROPHAZARD_models,
-                              function(x){ 
-                                x <- summary(x)
-                                p.value<-signif(x$wald["pvalue"], digits=5)
-                                wald.test<-signif(x$wald["test"], digits=5)
-                                beta<-signif(x$coef[1], digits=5);#coeficient beta
-                                HR <-signif(x$coef[2], digits=5);#exp(beta)
-                                HR.confint.lower <- signif(x$conf.int[,"lower .95"], 5)
-                                HR.confint.upper <- signif(x$conf.int[,"upper .95"], 5)
-                                HR <- paste0(HR, " (", HR.confint.lower, "-", HR.confint.upper, ")")
-                                res<-c(beta, HR, wald.test, p.value)
-                                names(res)<-c("beta", "HR (95% CI for HR)", "wald.test", "p.value")
-                                return(res)
-                                #return(exp(cbind(coef(x),confint(x))))
-                              })
+  coxph<-coxph(Surv(df$COXPROPHAZARDTIMETOEVENT,df$COXPROPHAZARDRIGHTCENSORING)~df$module_mean)
+  coxphs<-summary(coxph)
+  p.value<-signif(coxphs$waldtest["pvalue"], digits=5)
+  wald.test<-signif(coxphs$waldtest["test"], digits=5)
+  beta<-signif(coxphs$coefficients[1], digits=5)
+  HR<-signif(coxphs$coefficients[2], digits=5)
+  HR.confint.lower <- signif(coxphs$conf.int[,"lower .95"], 5)
+  HR.confint.upper <- signif(coxphs$conf.int[,"upper .95"], 5)
+  HR <- paste0(HR, " (", HR.confint.lower, "-", HR.confint.upper, ")")
+  COXPROPHAZARD_res<-c(beta, HR, wald.test, p.value)
+  names(COXPROPHAZARD_res)<-c("beta", "HR (95% CI for HR)", "wald.test", "p.value")
   COXPROPHAZARD_results <- t(as.data.frame(COXPROPHAZARD_res, check.names = FALSE,stringsAsFactors=FALSE))
+  # COXPROPHAZARD_formulas <- sapply(c("xx"), function(x) as.formula(paste('Surv(COXPROPHAZARDTIMETOEVENT, COXPROPHAZARDRIGHTCENSORING)~', x)))
+  # COXPROPHAZARD_models <- lapply(COXPROPHAZARD_formulas, function(x){coxph(x, data = df)})
+  # COXPROPHAZARD_res <- lapply(COXPROPHAZARD_models,
+  #                             function(x){ 
+  #                               x <- summary(x)
+  #                               p.value<-signif(x$wald["pvalue"], digits=5)
+  #                               wald.test<-signif(x$wald["test"], digits=5)
+  #                               beta<-signif(x$coef[1], digits=5);#coeficient beta
+  #                               HR <-signif(x$coef[2], digits=5);#exp(beta)
+  #                               HR.confint.lower <- signif(x$conf.int[,"lower .95"], 5)
+  #                               HR.confint.upper <- signif(x$conf.int[,"upper .95"], 5)
+  #                               HR <- paste0(HR, " (", HR.confint.lower, "-", HR.confint.upper, ")")
+  #                               res<-c(beta, HR, wald.test, p.value)
+  #                               names(res)<-c("beta", "HR (95% CI for HR)", "wald.test", "p.value")
+  #                               return(res)
+  #                               #return(exp(cbind(coef(x),confint(x))))
+  #                             })
+  # COXPROPHAZARD_results <- t(as.data.frame(COXPROPHAZARD_res, check.names = FALSE,stringsAsFactors=FALSE))
   return(COXPROPHAZARD_results)
 }
 
@@ -267,7 +281,7 @@ all_modules_SPEARMANCOR_test<-function(AMARETTOinit,AMARETTOresults,test_sample_
     featureMatrix<-create_feature_matrix(AMARETTOinit,AMARETTOresults,sample_annotation_df=test_sample_annotations,module_number=module_number,Sample='Sample')
     spearmancorr_vector<-featureMatrix%>%dplyr::pull(colnames(test_sample_annotations)[2])  
     df=data.frame(module_average=featureMatrix$mean_all,SPEARMANCORRTEST=spearmancorr_vector)
-    SPEARMANCORRTEST_results <- cor.test(~ SPEARMANCORRTEST + module_average, data=df, method = "spearman", alternative = "two.sided")
+    SPEARMANCORRTEST_results <- cor.test(df$module_average,df$SPEARMANCORRTEST, method = "spearman", alternative = "two.sided")
     df_result<-rbind(df_result,SPEARMANCORRTEST_results)
   }
   df_result<-as.data.frame(df_result,stringsAsFactors=FALSE)%>%rownames_to_column()
@@ -294,7 +308,7 @@ all_modules_PEARSONCOR_test<-function(AMARETTOinit,AMARETTOresults,test_sample_a
     featureMatrix<-create_feature_matrix(AMARETTOinit,AMARETTOresults,sample_annotation_df=test_sample_annotations,module_number=module_number,Sample='Sample')
     pearsoncorr_vector<-featureMatrix%>%dplyr::pull(colnames(test_sample_annotations)[2])  
     df=data.frame(module_average=featureMatrix$mean_all,PEARSONCORRTEST=pearsoncorr_vector)
-    PEARSONCORRTEST_results <- cor.test(~ PEARSONCORRTEST + module_average, data=df, method = "pearson",  conf.level = 0.95, alternative = "two.sided")
+    PEARSONCORRTEST_results <- cor.test(df$module_average,df$PEARSONCORRTEST, method = "pearson",  conf.level = 0.95, alternative = "two.sided")
     df_result<-rbind(df_result,PEARSONCORRTEST_results)
   }
   df_result<-as.data.frame(df_result,stringsAsFactors=FALSE)%>%rownames_to_column()
@@ -319,10 +333,16 @@ all_modules_PEARSONCOR_test<-function(AMARETTOinit,AMARETTOresults,test_sample_a
 all_module_WILCOXONRANKSUM_test<-function(AMARETTOinit,AMARETTOresults,test_sample_annotations){
   df_result<-NULL
   for (module_number in 1:AMARETTOresults$NrModules){
+    
     featureMatrix<-create_feature_matrix(AMARETTOinit,AMARETTOresults,sample_annotation_df=test_sample_annotations,module_number=module_number,Sample='Sample')
     wilcoxonsum_vector<-featureMatrix%>%dplyr::pull(colnames(test_sample_annotations)[2])  
-    df=data.frame(module_average=featureMatrix$mean_all,WILCOXONRANKSUMTEST=as.numeric(wilcoxonsum_vector))
-    WILCOXONRANKSUMTEST_results <- wilcox.test(x=df$module_average,y=df$WILCOXONRANKSUMTEST, alternative = "two.sided", paired = FALSE, conf.int = TRUE, conf.level = 0.95)
+    df<-data.frame(mean_all=featureMatrix$mean_all,wilcoxonsum_vector=as.factor(wilcoxonsum_vector),stringsAsFactors = FALSE)%>%drop_na()
+    if(length(unique(df$wilcoxonsum_vector))!=2){
+      stop(paste0("There is(are) ",length(unique(df$wilcoxonsum_vector)), " group(s) for ", colnames(test_sample_annotations)[2], ". Requiers two groups. The module Test is skiped."))
+    }
+    group1<-df$mean_all[df$wilcoxonsum_vector==unique(df$wilcoxonsum_vector)[1]]
+    group2<-df$mean_all[df$wilcoxonsum_vector==unique(df$wilcoxonsum_vector)[2]]
+    WILCOXONRANKSUMTEST_results <- wilcox.test(x=group1,y=group2, alternative = "two.sided", paired = FALSE, conf.int = TRUE, conf.level = 0.95)
     df_result<-rbind(df_result,WILCOXONRANKSUMTEST_results)
   }
   df_result<-as.data.frame(df_result,stringsAsFactors=FALSE)%>%rownames_to_column()
@@ -346,10 +366,17 @@ all_module_WILCOXONRANKSUM_test<-function(AMARETTOinit,AMARETTOresults,test_samp
 all_module_WILCOXONRANKSUMPAIRED_test<-function(AMARETTOinit,AMARETTOresults,test_sample_annotations){
   df_result<-NULL
   for (module_number in 1:AMARETTOresults$NrModules){
+    
     featureMatrix<-create_feature_matrix(AMARETTOinit,AMARETTOresults,sample_annotation_df=test_sample_annotations,module_number=module_number,Sample='Sample')
     wilcoxonsum_vector<-featureMatrix%>%dplyr::pull(colnames(test_sample_annotations)[2])  
-    df=data.frame(module_average=featureMatrix$mean_all,WILCOXONRANKSUMTESTPAIRED=as.numeric(wilcoxonsum_vector))
-    WILCOXONRANKSUMTESTPAIRED_results <- wilcox.test(x=df$module_average,y=df$WILCOXONRANKSUMTESTPAIRED, alternative = "two.sided", paired = TRUE, exact = NULL, correct = TRUE, conf.int = FALSE, conf.level = 0.95)
+    df<-data.frame(mean_all=featureMatrix$mean_all,wilcoxonsum_vector=as.factor(wilcoxonsum_vector),stringsAsFactors = FALSE)%>%drop_na()
+    if(length(unique(df$wilcoxonsum_vector))!=2){
+      stop(paste0("There is(are) ",length(unique(df$wilcoxonsum_vector)), " group(s) for ", colnames(test_sample_annotations)[2], ". Requiers two groups. The module Test is skiped."))
+    }
+    group1<-df$mean_all[df$wilcoxonsum_vector==df$wilcoxonsum_vector[[1]]]
+    group2<-df$mean_all[df$wilcoxonsum_vector==df$wilcoxonsum_vector[[2]]]
+    
+    WILCOXONRANKSUMTESTPAIRED_results <- wilcox.test(x=group1,y=group2, alternative = "two.sided", paired = TRUE, exact = NULL, correct = TRUE, conf.int = FALSE, conf.level = 0.95)
     df_result<-rbind(df_result,WILCOXONRANKSUMTESTPAIRED_results)
   }
   df_result<-as.data.frame(df_result,stringsAsFactors=FALSE)%>%rownames_to_column()
@@ -375,7 +402,7 @@ all_module_KRUSKALWALLIS_test<-function(AMARETTOinit,AMARETTOresults,test_sample
   for (module_number in 1:AMARETTOresults$NrModules){
     featureMatrix<-create_feature_matrix(AMARETTOinit,AMARETTOresults,sample_annotation_df=test_sample_annotations,module_number=module_number,Sample='Sample')
     kruskalwallis_vector<-featureMatrix%>%dplyr::pull(colnames(test_sample_annotations)[2])  
-    df=data.frame(module_average=featureMatrix$mean_all,KRUSKALWALLISTEST=kruskalwallis_vector)
+    df=data.frame(module_average=featureMatrix$mean_all,KRUSKALWALLISTEST=as.factor(kruskalwallis_vector))
     KRUSKALWALLISTEST_results <- kruskal.test(x=df$module_average,g=df$KRUSKALWALLISTEST)
     df_result<-rbind(df_result,KRUSKALWALLISTEST_results)
   }
@@ -383,11 +410,11 @@ all_module_KRUSKALWALLIS_test<-function(AMARETTOinit,AMARETTOresults,test_sample
   df_result$p.value<-as.numeric(unlist(df_result$p.value))
   df_result$p.value<-signif(df_result$p.value, digits=5)
   q.value<- p.adjust(df_result$p.value,method = "BH")
-  df_result<-add_column(df_result,q.value=q.value,ModuleNr=1:AMARETTOresults$NrModules)
+  df_result<-df_result%>%mutate(q.value=q.value)%>%mutate(ModuleNr=1:AMARETTOresults$NrModules)
   return(df_result)
 }
 
-#' Title
+#' Title all_module_TIMESERIESEDGE_test
 #'
 #' @param AMARETTOinit 
 #' @param AMARETTOresults 
@@ -403,13 +430,17 @@ all_module_TIMESERIESEDGE_test<-function(AMARETTOinit,AMARETTOresults,test_sampl
     featureMatrix<-create_feature_matrix(AMARETTOinit,AMARETTOresults,sample_annotation_df=test_sample_annotations,module_number=module_number,Sample='Sample')
     ModulesPhenotypes<-cbind(ModulesPhenotypes,featureMatrix$mean_all)
   }
+  #View(ModulesPhenotypes)
   mods<-t(ModulesPhenotypes)
   cov.time<-featureMatrix%>%dplyr::pull(colnames(test_sample_annotations)[2])  
   cov.cond<-featureMatrix%>%dplyr::pull(colnames(test_sample_annotations)[3])
+  length(cov.time)
+  length(cov.cond)
+  dim(mods)
   # cov.time<-TIMESERIESEDGETIME
   # cov.cond<-TIMESERIESEDGECONDITION
   # cov.replicate<-TIMESERIESEDGETIMEREPLICATE
-  edge_obj <- edge::build_study(data = as.matrix(mods), adj.var = cov.cond, tme = cov.time, ind = NULL,sampling = "timecourse")
+  edge_obj <- edge::build_study(data = as.matrix(mods), adj.var = as.factor(cov.cond), tme = as.numeric(cov.time),sampling = "timecourse")
   #edge_obj <- build_study(data = as.matrix(mods), adj.var = cov.cond, tme = cov.time, ind = cov.replicate, sampling = "timecourse")
   #de_obj <- edge_obj 
   #full_matrix<-fullModel(de_obj)
